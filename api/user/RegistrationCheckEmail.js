123fe2ae5b48aba03d3cbe1a748622226f9af701
Eaglorn <lexus1.45@mail.ru>
2023-04-08 21:49:03 +1000

    update

--- api/user/RegistrationCheckEmail.js
+++ api/user/RegistrationCheckEmail.js
@@ -2,7 +2,41 @@ var logger = require('../../logger');
 const prisma = require('../../db');
 var DateTime = require('luxon').DateTime;
 
-// TODO Сделать обработку кода на подверждение регистрации
 module.exports = async function (req, res) {
-
+  prisma.registration_check
+    .findFirst({
+      where: {
+        email: req.body.email,
+      },
+    })
+    .then((result) => {
+      if (result != null) {
+        if (result.code === req.body.code) {
+          prisma.user
+            .create({
+              data: {
+                email: result.email,
+                password: result.password,
+                type: 'user',
+              },
+            })
+            .then(() => {
+              res.send({
+                success: true,
+              });
+            })
+            .catch((err) => {
+              logger.error(err);
+            });
+        } else {
+          res.send({
+            success: false,
+          });
+        }
+      } else {
+        res.send({
+          success: false,
+        });
+      }
+    });
 };
