1ea077c8cfe7a6d6fcc45c658784cdc4cf5452fb
Eaglorn <lexus1.45@mail.ru>
2024-04-26 16:27:13 +1000

    Update socket.js

--- src/app/socket.js
+++ src/app/socket.js
@@ -4,12 +4,16 @@ let countPlayers = 0;
 
 module.exports = function (io) {
   io.on('connection', async (socket) => {
+    console.log(countPlayers);
     countPlayers++;
+    console.log(countPlayers);
     socket.emit('ChangeCountPlayer', {countPlayers: countPlayers});
   });
 
   io.on('disconnect', async (socket) => {
+    console.log(countPlayers);
     countPlayers--;
+    console.log(countPlayers);
     socket.emit('ChangeCountPlayer', {countPlayers: countPlayers});
   });
 };
