22a346f11696b37512ce43bdf526a474a0eae59e
Eaglorn <lexus1.45@mail.ru>
2024-04-26 22:52:10 +1000

    Update nodemailer.js

--- util/nodemailer.js
+++ util/nodemailer.js
@@ -3,7 +3,7 @@ const nodemailer = require('nodemailer');
 const transporter = nodemailer.createTransport({
   host: 'mail.customstory.ru',
   port: 587,
-  secure: false,
+  secure: true,
   auth: {
       user: 'registration@customstory.ru',
       pass: '1352461324qQ'
