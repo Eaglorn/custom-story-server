6c4e19fdd2621409ec260b69e16b48d0f69eafb1
Eaglorn <lexus1.45@mail.ru>
2024-05-20 12:07:09 +1000

    update

--- src/socket.js
+++ src/socket.js
@@ -1,16 +1,23 @@
 const logger = require('./logger')
+const io = require('@pm2/io')
 
 let PlayersCount = 0
+
+const userOnline = io.metric({
+  name: 'UserOnline',
+})
 
 module.exports = function (io) {
   io.on('connection', async (socket) => {
     PlayersCount++
     socket.emit('GetPlayersCount', { value: PlayersCount })
     socket.broadcast.emit('AddPlayersCount')
+    userOnline.set(PlayersCount)
 
     socket.on('disconnect', (reason) => {
       PlayersCount--
       io.sockets.emit('RemovePlayersCount')
+      userOnline.set(PlayersCount)
     })
   })
 }
