3ec4ac596ef5fa0ec6d219373230a2c3fc92bd87
Eaglorn <lexus1.45@mail.ru>
2024-06-20 12:08:46 +1000

    update

--- src/api/user/registration/CheckCode.js
+++ src/api/user/registration/CheckCode.js
@@ -2,27 +2,45 @@ const logger = require('../../../logger')
 const db = require('../../../db')
 const md5 = require('md5')
 
-module.exports = async function (req, res) {
+module.exports = function (req, res) {
   const redisEmail = 'user:registration:check:' + req.body.email
-
-  if (await db.redis.exists(redisEmail)) {
-    const registrationCheck = await db.redis.hgetall(redisEmail)
-    if (
-      registrationCheck.code === req.body.code &&
-      md5(req.body.password) === registrationCheck.password
-    ) {
-      await db.redis.hset(redisEmail, { type: 'history_read' })
-      res.send({
-        success: true,
-      })
-    } else {
-      res.send({
-        success: false,
-      })
-    }
-  } else {
-    res.send({
-      success: false,
+  db.redis
+    .exists(redisEmail)
+    .then((result) => {
+      if (result != 0) {
+        db.redis
+          .hgetall(redisEmail)
+          .then((registrationCheck) => {
+            if (
+              registrationCheck.code === req.body.code &&
+              md5(req.body.password) === registrationCheck.password
+            ) {
+              db.redis
+                .hset(redisEmail, { type: 'history_read' })
+                .then(() => {
+                  res.send({
+                    success: true,
+                  })
+                })
+                .catch((error) => {
+                  logger.log('error', error)
+                })
+            } else {
+              res.send({
+                success: false,
+              })
+            }
+          })
+          .catch((error) => {
+            logger.log('error', error)
+          })
+      } else {
+        res.send({
+          success: false,
+        })
+      }
     })
-  }
+    .catch((error) => {
+      logger.log('error', error)
+    })
 }
--- src/api/user/registration/HistoryRead.js
+++ src/api/user/registration/HistoryRead.js
@@ -2,20 +2,37 @@ const logger = require('../../../logger')
 const db = require('../../../db')
 const md5 = require('md5')
 
-module.exports = async function (req, res) {
-  try {
-    const redisEmail = 'user:registration:check:' + req.body.email
-    if (await db.redis.exists(redisEmail)) {
-      const registrationCheck = await db.redis.hgetall(redisEmail)
-      if (md5(req.body.password) === registrationCheck.password) {
-        await db.redis.hdel(redisEmail, 'type')
-        await db.redis.hsetnx(redisEmail, 'type', 'hero_create')
-        res.send({
-          success: true,
-        })
+module.exports = function (req, res) {
+  const redisEmail = 'user:registration:check:' + req.body.email
+  db.redis
+    .exists(redisEmail)
+    .then((result) => {
+      if (result != 0) {
+        db.redis
+          .hgetall(redisEmail)
+          .then((registrationCheck) => {
+            if (
+              registrationCheck.code === req.body.code &&
+              md5(req.body.password) === registrationCheck.password
+            ) {
+              db.redis
+                .hset(redisEmail, { type: 'hero_create' })
+                .then(() => {
+                  res.send({
+                    success: true,
+                  })
+                })
+                .catch((error) => {
+                  logger.log('error', error)
+                })
+            }
+          })
+          .catch((error) => {
+            logger.log('error', error)
+          })
       }
-    }
-  } catch (error) {
-    logger.log('error', error)
-  }
+    })
+    .catch((error) => {
+      logger.log('error', error)
+    })
 }
